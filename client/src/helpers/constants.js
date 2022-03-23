06a56338626939b8e85cb8cd6cbe35251d9af0c6
raphaelkhan8 <raphaelkhan8@gmail.com>
2022-03-23 15:42:13 -0500

    added intital team object

--- client/src/helpers/constants.js
+++ client/src/helpers/constants.js
@@ -11,4 +11,27 @@ const positions = {
     CENTER: "C",
 };
 
-export { nbaSeasons, positions }
+const initialTeam = {
+    players: [],
+    teamWins: 0,
+    offEfficiency: 0,
+    defEfficiency: 0,
+    teamMinutesLeft: 19680,
+    totalMinutesPlayed: 0,
+    totalShootingPercentage: 0,
+    twoPtMade: 0,
+    twoPtAttempt: 0,
+    threePtMade: 0,
+    threePtAttempt: 0,
+    pointsPG: 0,
+    assistsPG: 0,
+    stealsPG: 0,
+    blocksPG: 0,
+    turnoversPG: 0,
+    personalFoulsPG: 0,
+    offRPG: 0,
+    defRPG: 0,
+    averageAge: 0,
+}
+
+export { nbaSeasons, positions, initialTeam }
